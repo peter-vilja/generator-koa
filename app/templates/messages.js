86a40bd3c5e35cc76192be31062c69882a04eacf
petervilja <peter.vilja@gmail.com>
2014-01-24 19:23:57 +0200

    fixed indetation

--- app/templates/messages.js
+++ app/templates/messages.js
@@ -11,30 +11,29 @@ var render = views(__dirname + '/../views', {
 });
 
 function *home() {
-	this.body = yield render('list', { 'messages': messages });
+  this.body = yield render('list', { 'messages': messages });
 }
 
-function *listJson() {
-	this.body = yield messages;
+function *list() {
+  this.body = yield messages;
 }
 
 function *fetch(id) {
-	var message = messages[id];
-	if (!message) {
+  var message = messages[id];
+  if (!message) {
     this.throw(404, 'message with id = ' + id + ' was not found');
   }
-	this.body = yield message;
+  this.body = yield message;
 }
 
 function *create() {
-	var message = yield parse(this);
-	var id = messages.push(message) - 1;
-	message.id = id;
-	this.redirect('/');
+  var message = yield parse(this);
+  var id = messages.push(message) - 1;
+  message.id = id;
+  this.redirect('/');
 }
 
 module.exports.home = home;
-module.exports.list = listJson;
+module.exports.list = list;
 module.exports.fetch = fetch;
 module.exports.create = create;
-module.exports.messages = messages;
